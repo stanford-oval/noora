6a1f83902b58cf95267e30f021c1f221e1628af7
masonwang025 <masonwang025@gmail.com>
2024-03-23 23:35:46 +0500

    finish global nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
+import Footer from "./_components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}</body>
+      <Footer />
     </html>
   );
 }

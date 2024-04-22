9811043dd238a1a56c74a634d6dd8680e30234d8
masonwang025 <masonwang025@gmail.com>
2024-04-22 04:38:15 -0700

    remove shadow on cta

--- src/app/_components/header/content/LoginCTA.tsx
+++ src/app/_components/header/content/LoginCTA.tsx
@@ -6,7 +6,7 @@ function LoginCTA() {
     <div className="flex-shrink-0">
       <Link href="/login">
         <button
-          className="items-center flex space-x-2 trans-300 relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full text-gray-700 bg-gray-300/50 shadow-sm hover:bg-gray-300/80"
+          className="items-center flex space-x-2 trans-300 relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full text-gray-700 bg-gray-300/50 hover:bg-gray-300/80"
         >
           <div>Log in</div> <ArrowRightIcon className="h-4 w-4" />
         </button>

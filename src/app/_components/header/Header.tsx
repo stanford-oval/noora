f3013b5596754dff39c283bd3393bbe02ca66802
masonwang025 <masonwang025@gmail.com>
2024-04-22 02:03:48 -0700

    footer done

--- src/app/_components/header/Header.tsx
+++ src/app/_components/header/Header.tsx
@@ -1,12 +1,13 @@
 "use client"
 
+import Link from "next/link";
 import HomeLogo from "./HomeLogo";
 import NavContent from "./content/NavContent";
 
 export default function Header() {
 
   return (
-    <nav className="z-20 w-screen bg-white shadow fixed">
+    <nav className="z-40 w-screen bg-white shadow fixed">
       <div className="container">
         {/* outer wrapper to separate home logo (left end) and content (right end) */}
         <div className="flex justify-between h-16 items-center">
